5f8a8fe1159110a6db4fd7daa5ca05528c07637a
Hamid_Keshavarz <hamid.keshavarz@informatik.hs-fulda.de>
2023-11-28 21:02:10 +0100

    Add IUserService

---
+++ ShortcutTrainerBackend/ShortcutTrainerBackend/Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using ShortcutTrainerBackend.Data.Models;
+
+namespace ShortcutTrainerBackend.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<Question>> GetUnansweredQuestionsAsync(int courseId, string language, string keyboardLayout);
+        Task<IEnumerable<Question>> GetIncorrectQuestionsAsync(int courseId, string language, string keyboardLayout);
+        Task<IEnumerable<Question>> GetCorrectQuestionsAsync(int courseId, string language, string keyboardLayout);
+        Task UpdateAnswerStatusAsync(int questionId, QuestionResult result);
+    }
+}

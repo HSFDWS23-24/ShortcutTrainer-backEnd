267e7ca774f30170a3749e7de1840d0698537377
Erik Köhler <erik.koehler@informatik.hs-fulda.de>
2023-12-03 19:45:48 +0100

    Added AddUserAsync and UpdateUserAsync

--- ShortcutTrainerBackend/ShortcutTrainerBackend/Services/UserService.cs
+++ ShortcutTrainerBackend/ShortcutTrainerBackend/Services/UserService.cs
@@ -22,5 +22,56 @@ namespace ShortcutTrainerBackend.Services
             }
             return await Task.FromResult(Enumerable.Empty<User>());
         }
+
+        public async Task<User> AddUserAsync(User newUser)
+        {
+            if (newUser == null)
+            {
+                throw new ArgumentNullException(nameof(newUser));
+            }
+
+            // Check if the user already exists
+            var existingUser = _mockDatabase.DataStore.FirstOrDefault(c => c.Id == newUser.Id);
+
+            if (existingUser != null)
+            {
+                //Check if the user should be updated, if already exists
+                throw new InvalidOperationException("User with the same ID already exists");
+            }
+
+            // Add the new user to the database
+            _mockDatabase.DataStore.Add(newUser);
+
+            // Simulate asynchronous operation with Task.Delay
+            await Task.Delay(1);
+
+            return newUser;
+        }
+
+        public async Task<User> UpdateUserAsync(User updatedUser)
+        {
+            if (updatedUser == null)
+            {
+                throw new ArgumentNullException(nameof(updatedUser));
+            }
+
+            // Find the user in the database
+            var existingUser = _mockDatabase.DataStore.FirstOrDefault(c => c.Id == updatedUser.Id);
+
+            if (existingUser == null)
+            {
+                throw new InvalidOperationException("User does not exist");
+            }
+
+            // Update the user properties in the DataStore
+            existingUser.Name = updatedUser.Name;
+            existingUser.Email = updatedUser.Email;
+            // Add other properties as needed
+
+            // Simulate asynchronous operation with Task.Delay
+            await Task.Delay(1);
+
+            return existingUser;
+        }
     }
 }

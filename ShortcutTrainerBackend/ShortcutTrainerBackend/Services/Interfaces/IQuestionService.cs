c6b0a0234793d7f5f172b90b9f6da33189260e69
Hamid-Keshavarz <149174625+Hamid-Keshavarz@users.noreply.github.com>
2024-01-22 21:15:40 +0100

    QuestionService transition (#34)
    * Question feature (#34)
    
    * QuestionController has been extended
    * IQuestionService has been extended
    * The following methods has been addes to the QuestionService:
    	GetQuestionsByStatus
    	GetQuestions
    	GetQuestionsAsync
    	GetUnansweredQuestions
    	GetUnansweredQuestionsAsync
    	GetIncorrectQuestions
    	GetIncorrectQuestionsAsync
    	GetCorrectQuestions
    	GetCorrectQuestionsAsync
    	UpdateQuestionStatusAsync
    
    * Removal of 'using ShortcutTrainerBackend.Data.Models;'

--- ShortcutTrainerBackend/ShortcutTrainerBackend/Services/Interfaces/IQuestionService.cs
+++ ShortcutTrainerBackend/ShortcutTrainerBackend/Services/Interfaces/IQuestionService.cs
@@ -1,15 +1,27 @@
-﻿using ShortcutTrainerBackend.Data.Models;
-using ShortcutTrainerBackend.Data.TransferObjects;
+﻿using ShortcutTrainerBackend.Data.TransferObjects;
 
 namespace ShortcutTrainerBackend.Services.Interfaces
 {
     public interface IQuestionService
     {
-        IEnumerable<DtoQuestion> GetQuestions(int courseId, string language, string system);
-        Task<IEnumerable<DtoQuestion>> GetQuestionsAsync(int courseId, string language, string system);
-        Task<IEnumerable<DtoQuestion>> GetUnansweredQuestionsAsync(QuestionParameter request);
-        Task<IEnumerable<DtoQuestion>> GetIncorrectQuestionsAsync(QuestionParameter request);
-        Task<IEnumerable<DtoQuestion>> GetCorrectQuestionsAsync(QuestionParameter request);
-        Task UpdateQuestionStatusAsync(int questionId, string questionStatus);
+
+        // GetQuestions
+        IEnumerable<DtoQuestion> GetQuestions(string? userId, int courseId, string language, string system);
+        Task<IEnumerable<DtoQuestion>> GetQuestionsAsync(string? userId, int courseId, string language, string system);
+
+        // GetUnansweredQuestions
+        IEnumerable<DtoQuestion> GetUnansweredQuestions(string? userId, int courseId, string language, string system);
+        Task<IEnumerable<DtoQuestion>> GetUnansweredQuestionsAsync(string? userId, int courseId, string language, string system);
+
+        // GetIncorrectQuestions
+        IEnumerable<DtoQuestion> GetIncorrectQuestions(string? userId, int courseId, string language, string system);
+        Task<IEnumerable<DtoQuestion>> GetIncorrectQuestionsAsync(string? userId, int courseId, string language, string system);
+
+        // GetCorrectQuestions
+        IEnumerable<DtoQuestion> GetCorrectQuestions(string? userId, int courseId, string language, string system);
+        Task<IEnumerable<DtoQuestion>> GetCorrectQuestionsAsync(string? userId, int courseId, string language, string system);
+
+        // UpdateQuestionStatusAsync
+        Task<bool> UpdateQuestionStatusAsync(string? userId, int questionId, string questionStatus);
     }
 }
